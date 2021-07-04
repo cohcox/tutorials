623c71a987f0ce1ce3288c545d7dfb88c838ed22
Cody Cox <ccox96@gmail.com>
2021-07-04 12:16:50 -0400

    add in player input and win/loss conditions

--- UnrealC++/TripleX/TripleX.cpp
+++ UnrealC++/TripleX/TripleX.cpp
@@ -8,17 +8,36 @@ int main()
     std::cout << "You need to enter the correct code to continue...";
 
     // Declare the 3 number code
-    const int a = 4;
-    const int b = 9;
-    const int c = 7;
+    const int CodeA = 4;
+    const int CodeB = 9;
+    const int CodeC = 7;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print out to CLI
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "You win!";
+    }
+    else
+    {
+        std::cout << "You died";
+    }
 
     return 0;
 }
