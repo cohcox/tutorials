e7a3a5b9355431dea2d06eeb67c7b1db3f784d2d
Cody Cox <ccox96@gmail.com>
2021-07-04 11:33:14 -0400

    add in initial comments

--- UnrealC++/TripleX/TripleX.cpp
+++ UnrealC++/TripleX/TripleX.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
+    // Print welcome message to terminal
     std::cout << "You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout << "You need to enter the correct code to continue...";
 
+    // Declare the 3 number code
     const int a = 4;
     const int b = 9;
     const int c = 7;
@@ -13,6 +15,7 @@ int main()
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    // Print out to CLI
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
