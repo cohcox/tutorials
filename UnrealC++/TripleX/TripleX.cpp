985894399a0cdaec7558c02e62b78f5371eb4fbe
Cody Cox <ccox96@gmail.com>
2021-07-04 11:21:25 -0400

    Initital project set up for TripleX CLI game

---
+++ UnrealC++/TripleX/TripleX.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct code to continue...";
+
+    int a = 4;
+    int b = 9;
+    int c = 7;
+
+    a = 7;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
+    return 0;
+}
