861e560b51ae66d79bc92b09e534aa8fec6b15aa
Cody Cox <ccox96@gmail.com>
2021-07-04 11:29:56 -0400

    add in new folders for UnityC# and Python

--- UnrealC++/TripleX/TripleX.cpp
+++ UnrealC++/TripleX/TripleX.cpp
@@ -6,14 +6,12 @@ int main()
     std::cout << std::endl;
     std::cout << "You need to enter the correct code to continue...";
 
-    int a = 4;
-    int b = 9;
-    int c = 7;
+    const int a = 4;
+    const int b = 9;
+    const int c = 7;
 
-    a = 7;
-
-    int sum = a + b + c;
-    int product = a * b * c;
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
     std::cout << std::endl;
     std::cout << sum << std::endl;
