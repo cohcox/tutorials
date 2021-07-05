49c1ee6fb61828f322747f1503cd13d1dfc57f57
Cody Cox <ccox96@gmail.com>
2021-07-05 08:01:03 -0400

    attach BCC component to terminal actor. Recieve and return user input. Fix long compile time

--- UnrealC++/BullCow/Source/BullCowGame/BullCowCartridge.cpp
+++ UnrealC++/BullCow/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cow Game!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
+    PrintLine(TEXT("Press Enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    PrintLine(Input);
 
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("Huzzah! You got it!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Not Quite")); 
+    }
 }
